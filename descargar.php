fe61c8033ca7f6251f9caf2a9721950a8121883a
Usuario <usuario@MacBook-Pro-de-Usuario.local>
2020-12-21 23:54:44 -0300

    lu-rama. santiagomalprofesor

--- descargar.php
+++ descargar.php
@@ -2,4 +2,12 @@
 include 'clases/cadena.php';
 $objetoCadena = new Cadena();
 $objetoCadena->descargarUnCsv();
+
+//esto es un comentario hecho por luciana
+
+/*
+Esto es un comentario multilinea
+Linea dos
+*/
  ?>
+
